f1a164fffb583b6cc359b808da1a0ee9c3df93aa
Meezan Faisal <meezanfaisal786@gmail.com>
2026-05-10 02:05:53 +0530

    2nd file added

---
+++ books.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+
+}
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 20,
+  },
+
+  author: {
+    type: String,
+  },
+
+  price: {
+    type: Number,
+    min: [1, "Price is too low for Amazon selling"],
+  },
+
+  discount: {
+    type: Number,
+    default: 0,
+  },
+
+  category: {
+    type: String,
+    enum: ["fiction", "non-fiction"],
+  },
+
+  genre: [String]
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+Book.findByIdAndUpdate("69ff9699af647756bb957a00", {price: -500}, {runValidators: true})
+.then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.log(err.errors.price.properties.message);
+});
+
+// let book1 = new Book({
+//   title: "Marvel Comics V2",
+//   price: 600,
+ 
+//   genre: ["comics", "superheros", "fiction"]
+// });
+
+// book1.save()
+// .then((res) => {
+//   console.log(res);
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
